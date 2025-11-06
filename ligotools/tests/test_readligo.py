bf1ac3ab4f45b70ddf59c0b109a589e4a3f10855
Lois Tanglao <loistanglao@berkeley.edu>
2025-11-06 00:55:32 +0000

    tests working with python -m pytest ligotools

--- ligotools/tests/test_readligo.py
+++ ligotools/tests/test_readligo.py
@@ -1,14 +1,22 @@
-from ligotools import read_frame
-from ligotools import read_hdf5
+from ligotools.readligo import loaddata
+from ligotools.readligo import read_hdf5
 import numpy as np
 
-def test_read_frame():
-    filename = 'hw3-loistanglao/data/H-H1_LOSC_4_V2-1126259446-32.hdf5'
-    ifo = 'H1'
-    assert read_frame(filename, ifo, readstrain=True, strain_chan=None, dq_chan=None, inj_chan=None) == ...
+def test_loaddata():
+    filename = 'data/H-H1_LOSC_4_V2-1126259446-32.hdf5'
+    strain, time, channel_dict = loaddata(filename)
+    assert isinstance(strain, np.ndarray), "Strain should be a NumPy array"
+    assert isinstance(time, np.ndarray), "Time should be a NumPy array"
+    assert isinstance(channel_dict, dict), "Channel dict should be a dictionary"
 
 def test_read_hdf5():
-    filename = 'hw3-loistanglao/data/H-H1_LOSC_4_V2-1126259446-32.hdf5'
+    filename = 'data/H-H1_LOSC_4_V2-1126259446-32.hdf5'
     strain, gpsStart, ts, qmask, shortnameList, injmask, injnameList = read_hdf5(filename)
+    
     assert isinstance(strain, np.ndarray)
     assert gpsStart == 1126259446
+    assert isinstance(ts, float)
+    assert isinstance(qmask, np.ndarray)
+    assert isinstance(shortnameList, list)
+    assert isinstance(injmask, np.ndarray)
+    assert isinstance(injnameList, list)
